192750fa94314b9ec4d362d6934cb8c6304dc332
Ayush Tiwari <84335414+ayushtiwari110@users.noreply.github.com>
2022-12-28 02:19:23 +0530

    added Local Alert List Item Component...

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import  LocalAlertItem from './components/LocalAlertItem';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is not the final home screen or any screen. this is just for testing of components...</Text>
+      <LocalAlertItem
+       VehicleNumber="UP 93 AD 9876"
+       date="10/12/2022"
+       time="22:50" 
+      />
+      <LocalAlertItem
+       VehicleNumber="UP 93 AD 9876"
+       date="10/12/2022"
+       time="22:50" 
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFE2D6',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/LocalAlertItem.tsx
@@ -0,0 +1,52 @@
+import { FC } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+interface Props {
+    VehicleNumber: string;
+    date: string;
+    time: string;
+}
+
+const LocalAlertItem: FC<Props> = ({ VehicleNumber, date, time }) => {
+    return (
+        <View style={styles.rootContainer}>
+            <Text style={styles.vehicleNumber}>{VehicleNumber}</Text>
+            <View style={styles.dateAndTime}>
+                <Text style={styles.dateTime}>{date}</Text>
+                <Text style={styles.dateTime}>{time}</Text>
+            </View>
+        </View>
+    );
+};
+
+export default LocalAlertItem;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF4B7',
+        borderWidth: 4,
+        borderColor: '#FF9A6F',
+        borderRadius: 10,
+        marginVertical: 8,
+        marginHorizontal: 12,
+        elevation: 10,
+    },
+    vehicleNumber: {
+        padding: 8,
+        fontSize: 24,
+        fontWeight: '700', // 700 is bold, 400 is normal, semibold is 600
+        color: '#B06A00',
+    },
+    dateAndTime: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 8,
+        paddingBottom: 2,
+    },
+    dateTime:{
+        fontSize: 24,
+        fontWeight: '500',
+        color: '#FD9800',
+    },
+});
