e50c3e795db5078ef902c84482a57d560c3c527e
Harsha Vardhan <hv2003789@gmail.com>
2022-12-29 03:39:15 +0530

    ..

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     elevation: 15,
   },
   cardText: {
-    fontSize: 44,
+    fontSize: 48,
     fontWeight: "800",
     color: "#AF6A03",
     textAlign: "center",
