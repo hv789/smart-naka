26743373bcf72a93b86312265c441f6d3ed3db50
Harsha Vardhan <hv2003789@gmail.com>
2023-01-05 19:47:20 +0530

    Added Input forms

---
+++ components/UI/colors.tsx
@@ -0,0 +1,17 @@
+const COLORS = {
+    white: '#fff',
+    black: '#000',
+    blue: '#5D5FEE',
+    grey: '#BABBC3',
+    light: '#edeef5',
+    // light: '#f2f3f7',
+    darkBlue: '#7978B5',
+    red: 'red',
+    yellow: '#FFC400',
+    brown: '#965A00',
+    darkBrown: '#926900',
+    lightBrown: '#fcf9f7',
+    lightBrown2: '#FFF4B7',
+    darkBrown2: '#AF6A03',
+  };
+  export default COLORS;
